b2414f559946e233cab9fff5ff5bd26225e48580
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-10-25 11:25:49 +0200

    Add controller create

---
+++ controller/index.js
@@ -0,0 +1,29 @@
+const formidable = require("formidable");
+
+const {create, readAll, remove} = require("../model/todo");
+
+exports.create = (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, async (err, fields) => {
+        const {description} = fields;
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        if (!description) {
+            return res.status(400).json({
+                error: "Description is required"
+            });
+        }
+        create(description, (err, result) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            res.status(201).json({
+                data: result.rows[0]
+            });
+        });
+    });
+};
