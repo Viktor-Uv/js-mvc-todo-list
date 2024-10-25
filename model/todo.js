c17e7e6098731a13b559e7a4acdb93e9999ee5ac
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-10-25 11:19:21 +0200

    Add callback

--- model/todo.js
+++ model/todo.js
@@ -1,17 +1,17 @@
 const pool = require("database");
 
-exports.create = (description) => {
+exports.create = (description, cb) => {
     pool.query("INSERT INTO todo(description) VALUES ($1) RETURNING *;", [
             description
-        ]);
+        ], cb);
 };
 
-exports.readAll = () => {
-    pool.query("SELECT * FROM todo;");
+exports.readAll = (cb) => {
+    pool.query("SELECT * FROM todo;", cb);
 };
 
-exports.remove = (todoId) => {
+exports.remove = (todoId, cb) => {
     pool.query("DELETE FROM todo WHERE todo_id = $1;", [
         todoId
-    ]);
+    ], cb);
 };
